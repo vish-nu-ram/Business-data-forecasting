e1ab0eee695f5c9a62c38c439d13747b0e8bdd9b
Marie Schürmann <91538085+mschuermann@users.noreply.github.com>
2021-10-29 21:34:15 +0100

    Update R_Code.R

--- R_Code.R
+++ R_Code.R
@@ -8,4 +8,7 @@ tail(df)
 library(stats)
 
 z <- ts(data = df$BO,  freq= 24)
-plot.ts(z[1:100000])
+plot.ts(z[1:100000], ylab = "Bid Price - Opening of Day") 
+#is there a way to change the x-axis back to the time format, e.g. in years or the dates?
+
+
