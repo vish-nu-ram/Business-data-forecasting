7dcb0555e84ede5cc8f591fe500737e2dbf102df
vish-nu-ram <vishnu.ram@hotmail.com>
2021-10-30 02:29:28 +0100

    Update R_Code.R

--- R_Code.R
+++ R_Code.R
@@ -8,7 +8,11 @@ tail(df)
 library(stats)
 
 z <- ts(data = df$BO,  freq= 24)
+<<<<<<< Updated upstream
 plot.ts(z[1:100000], ylab = "Bid Price - Opening of Day") 
 #is there a way to change the x-axis back to the time format, e.g. in years or the dates?
 
 
+=======
+plot.ts(z[80000:100000])
+>>>>>>> Stashed changes
