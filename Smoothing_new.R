949ca72e6eb0a1950d48a34d82c085d3dc160762
Marie Schürmann <91538085+mschuermann@users.noreply.github.com>
2021-11-02 21:29:27 +0000

    Update Smoothing_new.R

--- Smoothing_new.R
+++ Smoothing_new.R
@@ -24,12 +24,12 @@ daywise1 <- daywise[1:350,]
 daywise_ts2 = daywise1[,-1]
 daywise_ts2 <- round(daywise_ts2,4)
 plot = plot.ts(daywise_ts2)
-daywise_ts = ts(round(daywise1$`mean(BO)`,4))
+daywise_ts = ts(round(daywise1$`mean(BO)`,4), frequency = 7)
 View(daywise_ts)
 
 #partioning the data
-train_data = ts(head(daywise_ts, n=250))
-valid_data = ts(tail(daywise_ts, n=100), start = 250, end = 350)
+train_data = ts(head(daywise_ts, n=300))
+valid_data = ts(tail(daywise_ts, n=50), start = 300, end = 350)
 
 class(valid_data)
 plot(train_data)
@@ -52,33 +52,30 @@ plot(exp_pred_MAN,  ylab = "Mean Bid Price at the beginning\n of each hour per d
      bty = "l", xaxt = "n", main = "multiplicative error, additive trend, and\n no seasonality")
 lines(exp_pred_MAN$fitted, lwd = 1, col = "blue")
 lines(valid_data)
-accuracy(exp_pred_MAN)
 accuracy(exp_pred_MAN, valid_data)
 
 # run Holt-Winters exponential smoothing
 # use ets() with option model = "MMN" to fit Holt-Winter's exponential smoothing
 exp_model_MMN <- ets(train_data, model = "MMN")
 # create predictions
-exp_pred_MMN <- forecast(exp_model_MMN, h=100, level = 0)
+exp_pred_MMN <- forecast(exp_model_MMN, h=length(valid_data), level = 0)
 # plot the series
-plot(exp_pred_MMN,  ylab = "Mean Bid Price at the beginning\n of each hour per day", xlab = "Time",
+plot(exp_pred_MMN,  ylab = "Mean Bid Price per day", xlab = "Time",
      bty = "l", xaxt = "n", main = "multiplicative error, multiplicative trend, and\n no seasonality")
 lines(exp_pred_MMN$fitted, lwd = 1, col = "blue")
 lines(valid_data)
-accuracy(exp_pred_MMN)
 accuracy(exp_pred_MMN, valid_data)
 
 # run Holt-Winters exponential smoothing
 # use ets() with option model = "AAN" to fit Holt-Winter's exponential smoothing
 exp_model_AAN <- ets(train_data, model = "AAN")
 # create predictions
-exp_pred_AAN <- forecast(exp_model_AAN, h=100, level = 0)
+exp_pred_AAN <- forecast(exp_model_AAN, h=length(valid_data), level = 0)
 # plot the series
 plot(exp_pred_AAN,  ylab = "Mean Bid Price at the beginning\n of each hour per day", xlab = "Time",
      bty = "l", xaxt = "n", main = "Holt-Winter's Exponential Smoothing\nadditive error, additive trend, and\n no seasonality")
 lines(exp_pred_AAN$fitted, lwd = 1, col = "blue")
 lines(valid_data)
-accuracy(exp_pred_AAN)
 accuracy(exp_pred_AAN, valid_data)
 
 # use ets() with option model = "MAN" to fit Holt-Winter's exponential smoothing
@@ -93,4 +90,10 @@ lines(valid_data)
 accuracy(exp_pred_ZZZ)
 accuracy(exp_pred_ZZZ, valid_data)
 
-
+###### prediction
+exp_model_pred <- ets(daywise_ts, model = "MAN")
+exp_pred_7days <- forecast(exp_model_MAN, h=7, level = 0)
+# plot the series
+plot(exp_pred_7days,  ylab = "Mean Bid Price at the beginning\n of each hour per day", xlab = "Time",
+     bty = "l", xaxt = "n", main = "multiplicative error, additive trend, and\n no seasonality")
+lines(exp_pred_7days$fitted, lwd = 1, col = "blue")
